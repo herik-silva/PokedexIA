ccadd6cebe4b8b13adc0353f935f459b0d05d654
herik-silva <herik.silva.019@gmail.com>
2025-02-20 10:41:36 -0300

    Fix

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ from core.utils.downloader import downloadModelFile
 
 downloadModelFile()
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == '__main__':
-    app.run(debug=True, host=ServerConfig.HOST.value, port=ServerConfig.PORT.value)
+    app.run(debug=True, host=ServerConfig.HOST.value, port=port)
