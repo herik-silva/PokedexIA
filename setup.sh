c4763520dbd52d3895db81959f92555f9b8fc4ba
herik-silva <herik.silva.019@gmail.com>
2025-03-08 13:18:03 -0300

    Added bruno's configurations and the setup file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,23 @@
-pip install -r requirements.txt
+mode=$1
 
-gunicorn -w 4 -b 0.0.0.0:5000 main:app
+if [ "$mode" = "help" ]; then
+    echo "Executa a instalação das dependências e inicia o servidor"
+    echo "Exemplo de uso: sh setup.sh gunicorn"
+    echo "Parametros:"
+    echo "- install: instala as dependências do projeto"
+    echo "- gunicorn: executado no modo de produção utilizando gunicorn"
+    echo "Por padrão é executado python main.py"
+    exit 1
+fi
+
+if [ "$mode" = "install"]; then
+    pip install --no-cache -r requirements.txt
+    exit 1
+fi
+
+if [ "$mode" = "prod" ]; then
+    gunicorn -w 4 -b 0.0.0.0:5000 main:app
+    exit 1
+fi
+
+python main.py
